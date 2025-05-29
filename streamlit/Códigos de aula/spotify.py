2d388fe420fd7b985f39bbcd2f38376b604c7944
Marissa Borges <marissaborges2006@gmail.com>
2025-05-29 16:19:35 -0300

    Streamlit: Introdução a biblioteca

---
+++ streamlit/Códigos de aula/spotify.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+from time import sleep
+
+st.set_page_config(
+    layout='wide',
+    page_title="Spotify Songs"
+)
+
+@st.cache_data
+def load_data():
+    df = pd.read_csv("streamlit/Códigos de aula/01 Spotify.csv")
+    sleep(5)
+    return df
+
+df = load_data()
+
+st.session_state['df_spotify'] = df
+
+df.set_index("Track", inplace=True)
+
+artists = df["Artist"].value_counts().index
+artist = st.sidebar.selectbox("Artista", artists)
+df_filtered = df[df["Artist"] == artist]
+
+albuns = df_filtered["Album"].value_counts().index
+album = st.selectbox("Album", albuns)
+
+df_filtered2 = df[df["Album"] == album]
+
+col1, col2 = st.columns([0.7, 0.3])
+
+col1.bar_chart(df_filtered2["Stream"])
+col2.line_chart(df_filtered2["Danceability"])
+
+st.write(artist)
+st.sidebar.button("test")
