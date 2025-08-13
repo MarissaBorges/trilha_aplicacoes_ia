ac6e2efde63e2867ed67a0ed576fc89039faf731
Marissa Borges <marissaborges2006@gmail.com>
2025-08-13 18:24:46 -0300

    Criando meu primeiro Agente

---
+++ agno/1.1.researcher.py
@@ -0,0 +1,13 @@
+from agno.agent import Agent
+from agno.tools.tavily import TavilyTools
+from dotenv import load_dotenv
+from agno.models.groq import Groq
+load_dotenv()
+
+agent = Agent(
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools=[TavilyTools()],
+    debug_mode=True
+)
+
+agent.print_response("Use suas ferramentas para pesquisar sobre o GPT-5 da OpenAI")
