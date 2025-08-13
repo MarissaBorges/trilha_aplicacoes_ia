6574700f36bd5030992a63522df32b0eae6a2f2f
Marissa Borges <marissaborges2006@gmail.com>
2025-08-13 18:25:20 -0300

    Criando meu primeiro Agente

---
+++ agno/1.2.analista.py
@@ -0,0 +1,13 @@
+from agno.agent import Agent
+from agno.tools.yfinance import YFinanceTools
+from agno.models.groq import Groq
+from dotenv import load_dotenv
+load_dotenv()
+
+agent = Agent(
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools=[YFinanceTools()],
+    instructions="Use tabelas para mostrar a informação final"
+)
+
+agent.print_response("Qual a cotação atual da APPLE?", stream=True)
